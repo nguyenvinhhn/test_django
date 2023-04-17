4d631862862a77ec10fe46b4c8164ec6f79025d7
nguyenvinhhn <nguyenvinh.hn.dev@gmail.com>
2023-04-17 04:27:06 +0000

    add runserve dev

--- src/mydjango/settings.py
+++ src/mydjango/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 's_-aro!sw@)bob$tojdq!s61$+3s22y=dbe!b5y3!p4ch&y3k#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ['192.168.11.128', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['web']
 
 
--- src/mydjango/urls.py
+++ src/mydjango/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('demo-new/', IndexView.as_view(), name='abc' ),
     path('demo-new-2/', IndexView.as_view(), name='abc' ),
     path('demo-new-4/', IndexView.as_view(), name='abc' ),
+    path('demo-new-5/', IndexView.as_view(), name='abc' ),
 ]
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
